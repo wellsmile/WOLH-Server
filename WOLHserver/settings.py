8605b471286b1210d0c91d8e1b5bf43d7f59e9fd
曹笑诚 <caoxiaocheng@ixianlai.com>
2018-06-20 11:27:03 +0800

    将本地与服务器端的数据库文件分离，以此保证服务器端的数据不受本地部署的影响

--- WOLHserver/settings.py
+++ WOLHserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import os
 
+windows_os=True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,11 +78,20 @@ WSGI_APPLICATION = 'WOLHserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
+if windows_os:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST_CHARSET': 'utf8',        
+        'TEST_COLLATION': 'utf8_general_ci',
+    },
+}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/opt/WOLHserver/uwsgi', 'db.sqlite3'),
         'TEST_CHARSET': 'utf8',        
         'TEST_COLLATION': 'utf8_general_ci',
     },
